87708ad28b2dfc949254b28347e757ead1f878d4
xadd <alexander.kozlovich@gmail.com>
2018-12-08 06:00:31 +0300

    0.2.1

--- src/libs/ace-api/getStreamFeatures.ts
+++ src/libs/ace-api/getStreamFeatures.ts
@@ -5,13 +5,17 @@ import { StreamFeatures } from "./types";
 async function getStreamFeatures(iproxyPath: string, cid: string, sid: string): Promise<StreamFeatures> {
     const url = `${iproxyPath}/ace/getstream?id=${cid}&.mp4&sid=${sid}&format=json`;
     const res = await fetch(url);
-    const json = await res.json();
+    const data = await res.json();
+
+    if (data.error) {
+        throw new Error(data.error);
+    }
 
     return {
-        playbackUrl: normalizeIProxyUrl(iproxyPath, json.response["playback_url"]),
-        statUrl: normalizeIProxyUrl(iproxyPath, json.response["stat_url"]),
-        commandUrl: normalizeIProxyUrl(iproxyPath, json.response["command_url"]),
-        eventUrl: normalizeIProxyUrl(iproxyPath, json.response["event_url"]),
+        playbackUrl: normalizeIProxyUrl(iproxyPath, data.response["playback_url"]),
+        statUrl: normalizeIProxyUrl(iproxyPath, data.response["stat_url"]),
+        commandUrl: normalizeIProxyUrl(iproxyPath, data.response["command_url"]),
+        eventUrl: normalizeIProxyUrl(iproxyPath, data.response["event_url"]),
     };
 }
 
